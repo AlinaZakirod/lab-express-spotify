535f6481b7b6fe580120fab601f6d7abcadf794d
Alina Zakirova <alinazakirova@reloquests-mbp.lan>
2019-08-11 21:21:52 -0400

    tracks left

--- starter_code/app.js
+++ starter_code/app.js
@@ -42,7 +42,54 @@ app.get('/home', (req,res,next) => {
   res.render('index');
 })
 
+app.get('/artists' , (req, res, next) => {
+  //console.log('This is the name' , req.query)
 
+  spotifyApi.searchArtists(req.query.artist)
+  .then(data => {
+   // console.log(data.body.artists.items);
+    res.render('artists', {data: data.body.artists.items})
+  })
+  .catch(err => {
+    console.log("The error while searching artists occurred: ", err);
+  })
+})
+
+app.get('/albums/:id', (req, res, next) => {
+  spotifyApi.getArtistAlbums(req.params.id)
+  .then(data => {
+    console.log("album id is:", data.body.items[0].id)
+    res.render('albums', {albums: data.body.items})
+  })
+  .catch(err =>{
+    console.log("The error while searching albums occurred: ", err);
+  })
+})
+
+app.get('/tracks/:albumId', (req, res, next) => {
+  
+  spotifyApi
+  .getAlbumTracks(req.params.albumId)
+  .then(data => {
+    console.log("show me tracks: ", data.body)
+    res.render('tracks', {tracks: data.body.items})
+  })
+  .catch(err => console.log("error displaying tracks", err))
+})
+
+
+// app.get('/home' , (req, res, next) => {
+//   console.log('This is the name' , req.query)
+//   spotifyApi.searchArtists(req.query.artist)
+
+//   .then(data => {
+//     console.log("The received data from the API: ", data);
+//     res.render('artists', {data})
+//   })
+//   .catch(err => {
+//     console.log("The error while searching artists occurred: ", err);
+//   })
+// })
 
 
 
